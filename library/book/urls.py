e84c58901df6f38a2d1ee085fa32888507adeda8
“Anton <shulezhko.antony@gmail.com>
2025-06-21 02:11:38 +0300

    path changed

--- library/book/urls.py
+++ library/book/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.all_books, name='all_books'),
-    path('<int:book_id>/', views.book_detail, name='book_detail'),
+    path('create/', views.create_book_view, name='book_create'),
     path('filter/', views.filter_books, name='filter_books'),
     path('user/<int:user_id>/', views.user_books, name='user_books'),
-    path('create/', views.create_book_view, name='book_create'),
+    path('<int:book_id>/', views.book_detail, name='book_detail'),
+    path('<int:book_id>/edit/', views.update_book_view, name='book_update'),
 ]
--- library/book/views.py
+++ library/book/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Book
+from .forms import BookForm 
 from order.models import Order
 
 def all_books(request):
@@ -9,6 +10,19 @@ def all_books(request):
 def book_detail(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
     return render(request, 'book/book_detail.html', {'book': book})
+
+def update_book_view(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_detail', book_id=book.id)
+    else:
+        form = BookForm(instance=book)
+
+    return render(request, 'book/book_update.html', {'form': form, 'book': book})
 
 def filter_books(request):
     books = Book.objects.all()
